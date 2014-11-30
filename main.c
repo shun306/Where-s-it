f57fac9ad43a7e1500d2fad3c3dbfdea72301181
helderdoutel <heldergomesdoutel@gmail.com>
2014-11-30 16:59:37 -0200

    Segunda Versao

--- main.c
+++ main.c
@@ -13,10 +13,12 @@
 const int LARGURA_TELA = 800;
 const int ALTURA_TELA = 600;
 
+void fadeout(int vel);
+void fadein(ALLEGRO_BITMAP *imagem, int vel);
 
-
-ALLEGRO_BITMAP *fundo_jogo = NULL, *mapa_br = NULL;
+ALLEGRO_BITMAP *fundo_jogo = NULL, *mapa_br = NULL;	
 ALLEGRO_DISPLAY *janela = NULL;
+ALLEGRO_FONT *font = NULL;
 
 int main(void){
 
@@ -27,7 +29,6 @@ int main(void){
 	ALLEGRO_BITMAP *tax_1 = NULL, *folha = NULL, *sombra = NULL, *sombra_folha = NULL;
 	ALLEGRO_AUDIO_STREAM *musica = NULL;
 	ALLEGRO_SAMPLE *sample = NULL;
-	ALLEGRO_FONT *font = NULL;
 	int sair = 0, teste = 0, jogar = 0, opcoes = 0, i=0, score=0;
 
 	// Inicializa a Allegro
@@ -72,8 +73,10 @@ int main(void){
 	// Desenhar abertura do Jogo
     al_attach_audio_stream_to_mixer(musica, al_get_default_mixer());
     al_set_audio_stream_playing(musica, true);
+	fadein(abertura, 1);
 	al_draw_bitmap(abertura, 0, 0, 0);
 	al_flip_display();
+	fadeout(2);
 
 	//Falar que os eventos vão vir do Mouse e janela
 	al_register_event_source(fila_eventos, al_get_mouse_event_source());
@@ -96,6 +99,10 @@ int main(void){
 			al_draw_bitmap(folha, 370, 420, 0);
 			al_draw_bitmap(tax_1, 380, 400, 0);
 			al_draw_bitmap(tax_1, 730, 400, 0);
+			al_draw_text(font, al_map_rgb(0, 0, 0), 440, 430, 0, "Alex Shun");
+			al_draw_text(font, al_map_rgb(0, 0, 0), 480, 457, 0, "Fernando Medin");
+			al_draw_text(font, al_map_rgb(0, 0, 0), 460, 481, 0, "Helder Doutel");
+			al_draw_text(font, al_map_rgb(0, 0, 0), 500, 510, 0, "Rodrigo Melo");
 			al_flip_display();
 			i=1;
 		}
@@ -111,7 +118,7 @@ int main(void){
 		if(evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN){
 
 			if(evento.mouse.x >= 30 &&
-					evento.mouse.x <= 330 &&
+					evento.mouse.x <= 330 && 
 					evento.mouse.y <= ALTURA_TELA - 10 &&
 					evento.mouse.y >= ALTURA_TELA - al_get_bitmap_height(botao_sair) - 10){
 
@@ -138,8 +145,10 @@ int main(void){
 				al_flip_display();
 				jogar = 1;
 				printf("vaiabrir\n");
+				al_destroy_bitmap(botao_jogar_press);
 				if(jogar == 1)
-					jogaar();
+					jogaar(fila_eventos, evento);
+				
 			}
 		}
 	}
@@ -153,9 +162,49 @@ int main(void){
 	al_destroy_bitmap(folha);
 	al_destroy_bitmap(tax_1);
 	al_destroy_bitmap(imagem);
+	al_destroy_display(janela);
 
 	// Finaliza a janela
 	return 0;
 }
 
+void fadein(ALLEGRO_BITMAP *imagem, int vel){
+	if(vel < 0){
+		vel = 1;
+	}
+	else if(vel > 15){
+		vel = 15;
+	}
 
+	int alfa;
+	for (alfa = 0; alfa <= 255; alfa += vel){
+		al_clear_to_color(al_map_rgb(0, 0, 0));
+		al_draw_tinted_bitmap(imagem, al_map_rgba(alfa, alfa, alfa, alfa), 0, 0, 0);
+		al_flip_display();
+		al_rest(0.005);
+	}
+}
+void fadeout(int vel){
+	ALLEGRO_BITMAP *buffer = NULL;
+	buffer = al_create_bitmap(LARGURA_TELA, ALTURA_TELA);
+	al_set_target_bitmap(buffer);
+	al_draw_bitmap(al_get_backbuffer(janela), 0, 0, 0);
+	al_set_target_bitmap(al_get_backbuffer(janela));
+
+	if (vel <= 0){
+		vel = 1;
+	}
+	else if (vel > 15){
+		vel = 15;
+	}
+
+	int alfa;
+	for (alfa = 0; alfa <= 255; alfa += vel){
+		al_clear_to_color(al_map_rgba(0, 0, 0, 0));
+		al_draw_tinted_bitmap(buffer, al_map_rgba(255 - alfa, 255 - alfa, 255 - alfa, alfa), 0, 0, 0);
+		al_flip_display();
+		al_rest(0.005);
+	}
+
+	al_destroy_bitmap(buffer);
+}
